f4b7fc0819120c19d91aaddfeb563d68b40a9a35
Gustavo Henrique <guazambujinha@gmail.com>
2026-07-27 15:36:19 -0300

    feat: Criando a regra de negócios

---
+++ .venv/controllers/financeiro_controller.py
@@ -0,0 +1,24 @@
+from database.fake_db import transacoes
+from models.transacao_model import Transacao
+
+
+def listar_transacoes():
+    return transacoes
+
+
+def criar_transacao(transacao: Transacao):
+    transacoes.append(transacao.model_dump())
+    return transacao
+
+
+def calculcar_transacao():
+    saldo = 0
+    for t in transacoes:
+        if t["tipo"] == "entrada":
+            saldo += t["valor"]
+        else:
+            saldo -= t["valor"]    
+        return {"saldo": saldo}
+
+
+ 
