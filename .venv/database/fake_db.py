98ad3ddac36d4c8779707a153a08dce6888d528d
Gustavo Henrique <guazambujinha@gmail.com>
2026-07-27 14:54:03 -0300

    feat: Criação banco em memória

---
+++ .venv/database/fake_db.py
@@ -0,0 +1,17 @@
+transacoes = [
+
+    {
+        "id": 1,
+        "descricao": "Salário",
+        "valor": 3000,
+        "tipo": "entrada",
+        "categoria": "Renda"
+    },
+    {
+        "id": 2,
+        "descricao": "Aluguel",
+        "valor": 3000,
+        "tipo": "saida",
+        "categoria": "Moradia"
+    }
+]
